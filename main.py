f9451b04f2fecad8b35aca1c6a754cd37b068ca8
james <61799526+james-stewart-808@users.noreply.github.com>
2026-03-30 10:52:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,27 +4,16 @@ from io import StringIO
 import streamlit as st
 st.header("Welcome to the Homepage of the International Shipping Dashboard 🏡")
 
-def load_original_data():
-    url = 'https://raw.githubusercontent.com/james-stewart-808/inventory-tracker/main/datasets/dom_inv_by_vess_type.csv'
-    response = requests.get(url)
-    if response.status_code == 200:
-        return pd.read_csv(StringIO(response.text), nrows=2)
-    else:
-        st.error("Failed to load data from GitHub.")
-        return None
-#country_iso_codes = load_original_data()
-
-#country_iso_codes = pd.read_csv("https://raw.githubusercontent.com/james-stewart-808/inventory-tracker/main/datasets/dom_inv_by_vess_type.csv")
 
 # Function to load the CSV file
-@st.cache_data(scope="session")
-def load_data(file):
-    data = pd.read_csv(file)
-    return data
+#@st.cache_data(scope="session")
+#def load_data(file_dir):
+#    data_dir = "https://raw.githubusercontent.com/james-stewart-808/inventory-tracker/main/"
+#    return pd.read_csv(data_dir + file_dir)
 
-country_iso_codes = load_data('https://raw.githubusercontent.com/james-stewart-808/inventory-tracker/main/datasets/dom_inv_by_vess_type.csv')
+#country_iso_codes = load_data('datasets/dom_inv_by_vess_type.csv')
 
-
+country_iso_codes = pd.read_csv("https://raw.githubusercontent.com/james-stewart-808/inventory-tracker/main/datasets/dom_inv_by_vess_type.csv")
 #country_iso_codes = pd.read_csv("/country_iso_codes.csv")#, usecols=country_iso_codes_cols, dtype=country_iso_codes_dtype).rename(columns=country_iso_codes_renames)
 #country_iso_codes.loc[country_iso_codes.iso_country == "Namibia", "iso_2"] = "NA"
 
