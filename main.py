fb4fedaa4257e24432955e1cef6d6e796eca29a9
james <61799526+james-stewart-808@users.noreply.github.com>
2026-03-30 10:52:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ st.header("Welcome to the Homepage of the International Shipping Dashboard 🏡"
 
 #country_iso_codes = load_data('datasets/dom_inv_by_vess_type.csv')
 
-country_iso_codes = pd.read_csv("https://raw.githubusercontent.com/james-stewart-808/inventory-tracker/main/datasets/dom_inv_by_vess_type.csv")
+country_iso_codes = pd.read_csv(
+  "https://raw.githubusercontent.com/james-stewart-808/inventory-tracker/main/datasets/dom_inv_by_vess_type.csv"
+)
 #country_iso_codes = pd.read_csv("/country_iso_codes.csv")#, usecols=country_iso_codes_cols, dtype=country_iso_codes_dtype).rename(columns=country_iso_codes_renames)
 #country_iso_codes.loc[country_iso_codes.iso_country == "Namibia", "iso_2"] = "NA"
 
