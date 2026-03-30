8251056e75d10c4886b2d74d1b90dfc8e79b851d
james <61799526+james-stewart-808@users.noreply.github.com>
2026-03-30 11:19:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Define the pages
 main = st.Page("main.py", title="Home", icon="🏡")
-inventories = st.Page("inventories/inventories.py", title="Voyage-based Inventories", icon="🚢")
+inventories = st.Page("inventories.py", title="Voyage-based Inventories", icon="🚢")
 #trade = st.Page("trade/trade.py", title="Merchandise Trade Portfolios", icon="📦")
 #impact_tracking = st.Page("impact_tracking/impact_tracking.py", title="Impact Tracking Results", icon="💵")
 #example_page = st.Page("streamlit_example.py", title="Streamlit Example", icon="❄️")
