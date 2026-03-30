28b0d73f000d4c1b954d942de700bad9df440bee
james <61799526+james-stewart-808@users.noreply.github.com>
2026-03-30 09:42:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # Define the pages
-#main = st.Page("main.py", title="Home", icon="🏡")
+main = st.Page("main.py", title="Home", icon="🏡")
 #inventories = st.Page("inventories/inventories.py", title="Voyage-based Inventories", icon="🚢")
 #trade = st.Page("trade/trade.py", title="Merchandise Trade Portfolios", icon="📦")
 #impact_tracking = st.Page("impact_tracking/impact_tracking.py", title="Impact Tracking Results", icon="💵")
@@ -10,7 +10,7 @@ import pandas as pd
 
 
 # Set up navigation
-pg = st.navigation()#([main])#, inventories, trade, impact_tracking]) #, example_page])
+pg = st.navigation([main])#, inventories, trade, impact_tracking]) #, example_page])
 
 # Run the selected page
 pg.run()
