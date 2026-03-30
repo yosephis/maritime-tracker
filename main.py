c236b6d6a0fabe7d5085f80848756ab63a58b154
james <61799526+james-stewart-808@users.noreply.github.com>
2026-03-30 10:00:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ st.header("Welcome to the Homepage of the International Shipping Dashboard 🏡"
 
 country_iso_codes = pd.read_csv("datasets/national_stats/country_iso_codes.csv")#, usecols=country_iso_codes_cols, dtype=country_iso_codes_dtype).rename(columns=country_iso_codes_renames)
 #country_iso_codes.loc[country_iso_codes.iso_country == "Namibia", "iso_2"] = "NA"
-display(country_iso_codes.head(2))
+print(country_iso_codes.head(2))
