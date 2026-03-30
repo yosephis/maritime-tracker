c621d76e7decfcb5637dcbabc47db400708c0c48
james <61799526+james-stewart-808@users.noreply.github.com>
2026-03-30 10:58:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ from io import StringIO
 import streamlit as st
 st.header("Welcome to the Homepage of the International Shipping Dashboard 🏡")
 
-country_iso_codes_cols = ["name", "alpha-2", "alpha-3", "country-code"]
-country_iso_codes_dtype = {"alpha-2":str, "alpha-3":str, "country-code":str}
-country_iso_codes_renames = {"name":"iso_country", "alpha-2":"iso_2", "alpha-3":"iso_3", "country-code":"iso_code"}
+#country_iso_codes_cols = ["name", "alpha-2", "alpha-3", "country-code"]
+#country_iso_codes_dtype = {"alpha-2":str, "alpha-3":str, "country-code":str}
+#country_iso_codes_renames = {"name":"iso_country", "alpha-2":"iso_2", "alpha-3":"iso_3", "country-code":"iso_code"}
+#country_iso_codes = pd.read_csv(
+#  "https://raw.githubusercontent.com/james-stewart-808/inventory-tracker/main/datasets/country_iso_codes.csv",
+#  usecols=country_iso_codes_cols, 
+#  dtype=country_iso_codes_dtype).rename(
+#  columns=country_iso_codes_renames)
+#country_iso_codes.loc[country_iso_codes.iso_country == "Namibia", "iso_2"] = "NA"
+
 country_iso_codes = pd.read_csv(
-  "https://raw.githubusercontent.com/james-stewart-808/inventory-tracker/main/datasets/country_iso_codes.csv",
-  usecols=country_iso_codes_cols, 
-  dtype=country_iso_codes_dtype).rename(
-  columns=country_iso_codes_renames)
-country_iso_codes.loc[country_iso_codes.iso_country == "Namibia", "iso_2"] = "NA"
+  "https://raw.githubusercontent.com/james-stewart-808/inventory-tracker/main/datasets/country_iso_codes.csv"
+)
 
 st.write(country_iso_codes.head())
 #
