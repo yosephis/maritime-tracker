5db0117d71ce85e21fbf45bfe45dd719ba767178
james <61799526+james-stewart-808@users.noreply.github.com>
2026-03-30 10:44:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ def load_original_data():
     else:
         st.error("Failed to load data from GitHub.")
         return None
-country_iso_codes = load_original_data()
+#country_iso_codes = load_original_data()
 
-#country_iso_codes = pd.read_csv("https://raw.githubusercontent.comjames-stewart-808/inventory-tracker/blob/1ecaad7e067e4d9016790fabe59a2c5691cfe764/datasets/dom_inv_by_vess_type.csv")
+country_iso_codes = pd.read_csv("https://raw.githubusercontent.com/james-stewart-808/inventory-tracker/main/datasets/dom_inv_by_vess_type.csv")
 
 # Function to load the CSV file
 #@st.cache_data
@@ -27,6 +27,6 @@ def load_data(file):
     data = pd.read_csv(file)
     return data
 
-#country_iso_codes = load_data('https://raw.githubusercontent.comjames-stewart-808/inventory-tracker/blob/1ecaad7e067e4d9016790fabe59a2c5691cfe764/datasets/dom_inv_by_vess_type.csv')
+#country_iso_codes = load_data('https://raw.githubusercontent.com/james-stewart-808/inventory-tracker/main/datasets/dom_inv_by_vess_type.csv')
 st.write(country_iso_codes)
 #
