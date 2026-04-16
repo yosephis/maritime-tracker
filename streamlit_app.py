1f2f4acfd70fcbb72b5c9502738a6f3dd310b418
yosephis <144414930+yosephis@users.noreply.github.com>
2026-04-16 15:09:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-st.sidebar.image("UCL_Logo_S_1C_B_RGB_Energy-Inst.png", width=200)
+sidebar = st.sidebar.image("UCL_Logo_S_1C_B_RGB_Energy-Inst.png", width=200)
 # Define the pages
 main = st.Page("main2.py", title="Home", icon="🏡")
 inventories = st.Page("inventories.py", title="Voyage-based Inventories", icon="🚢")
@@ -8,7 +8,7 @@ trade = st.Page("trade.py", title="Merchandise Trade Portfolios", icon="📦")
 impact_tracking = st.Page("impact_tracking.py", title="Impact Tracking Results", icon="💵")
 
 # Set up navigation
-pg = st.navigation([main, inventories, trade, impact_tracking])
+pg = st.navigation([sidebar, main, inventories, trade, impact_tracking])
 
 # Run the selected page
 pg.run()
