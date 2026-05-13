97a3a63dd7317a48d7686ca938cd2083dec19de0
yosephis <144414930+yosephis@users.noreply.github.com>
2026-05-13 19:40:32 +0100

    Update gdp_change.py

--- gdp_change.py
+++ gdp_change.py
@@ -4,4 +4,16 @@ import pandas as pd
 
 st.title('National Macroeconomic Effects of Climate Scenarios')
 
+df_profile = 'gdp_change'
+df_1 = pd.read_csv("https://raw.githubusercontent.com/yosephis/maritime-tracker/main/datasets/portfolios/{0}/{1}.csv".format(\ st.session_state.iso_code, df_profile))
 
+filtered_df1 = df_1[df_1['year'].isin(['2030','2040','2050','2060','2070','2080','2090','2100'])]
+
+fig=px.bar(filtered_df1, x='year',
+           y='gdp_per_c', 
+           color='Scenario',
+           barmode='group',
+           text_auto=True)
+
+# Render the interactive chart inside Streamlit
+st.plotly_chart(fig, use_container_width=True)
